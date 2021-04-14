a4b23e86eebac4e394083f6ff325ba9196c98e35
marion <marion070189@hotmail.fr>
2021-04-14 12:12:35 +0200

    création d'un fichier csv par catégorie / reste une erreur

--- code_all_category.py
+++ code_all_category.py
@@ -2,10 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from math import *
 import wget
-from fonction_scrapping import scrapping_one_book, scrapping_all_category, scrapping_one_category, scrapping_images
+from fonction_scrapping import scrapping_one_book, scrapping_all_category, scrapping_one_category, scrapping_images, scrapp_category
 import os.path
-os.makedirs("images_Books")
-output_directory = "images_Books"
+#os.makedirs("images_Books")
+#output_directory = "images_Books"
+import csv
+import re
 
 url_site = 'http://books.toscrape.com/index.html'
 liste_urls_completes = []
@@ -14,26 +16,31 @@ if response.ok:
     liste_urls_incompletes = scrapping_all_category(url_site) #variable qui contient le résultat de la fonction
     for url_incomplete in liste_urls_incompletes:
         liste_urls_completes.append('http://books.toscrape.com/' + url_incomplete) #on rajoute ds liste_urls_completes, le http et urls incomplets
-    print(liste_urls_completes)         
+    #print(liste_urls_completes)       
 #1 : affiche les liens de toutes les catégories sous forme de liste
-    
+
+
 links_livres = []
-for url_complete in liste_urls_completes[40:]: #tant qu'on est dans les urls des catégories #supprime le premier (page d'accueil)
+for url_complete in liste_urls_completes[1:3]: #tant qu'on est dans les urls des catégories #supprime le premier (page d'accueil)
     links_livres.append(scrapping_one_category(url_complete)) #rajoute chaque lien dans la liste/ url_complete = les urls des catégories
-    print(links_livres)
+print(links_livres)
 #2 : affiche les urls de tous les livres des catégories sous forme de liste 
 
 book_info = []
 for link_livre in links_livres:
     for link_dun_livre in link_livre:   
         book_info.append(scrapping_one_book(link_dun_livre))
-print(book_info)
+#print(book_info)
 
-with open ('infos.csv', 'a+') as file:
-        file.write('product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url\n')
-        file.write(str(book_info))
 
-for link_livre in links_livres:
+for url_complete in liste_urls_completes[1:3]: 
+    with open ('infos_' + str((scrapp_category(url_complete)))+ '.csv', 'w') as file:
+            file.write('product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url\n')
+            file.write(str(book_info))
+
+
+
+"""for link_livre in links_livres:
     for link_dun_livre in link_livre:
         urls_image = scrapping_images(link_dun_livre)
     print(urls_image)
