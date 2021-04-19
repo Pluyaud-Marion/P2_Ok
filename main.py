a783eb885d86a41bcdbc423bfdbb0e24cb13463a
marion <marion070189@hotmail.fr>
2021-04-19 11:59:30 +0200

    code commenté ok

--- main.py
+++ main.py
@@ -37,35 +37,5 @@ for url_complete in liste_urls_completes[1:]:
     os.chdir("../..")
 
     
-"""
-1/BOUCLE 1 : pour chaque url de catégorie ds tous les urls de catégorie
--création dossier catégorie
--appelle fonction 3 : récupère les liens de tous les livres de la catégorie
--appelle fonction 6 : met les infos de chaque livre ds fichier csv
--ds le dossier de la catégorie --> création dossier images_catégorie
--ds le dossier images_catégorie =
-2/BOUCLE 2 : pour chaque livre ds tous les livres de la catégorie
--appelle fonction 5 : récupère les urls des images de la catégorie
--télécharge les images à partir des urls
-3/boucle finie pour une catégorie --> on remonte au dossier parent x 2 et on recommence la boucle sur une autre catégorie
-"""
 
-"""
-1/création dossier books_toscrap
-va à l'intérieur
-2/pour chaque url de catégorie ds tous les urls de catégories création dossier de la catégorie
--va à l'intérieur
--on y met le csv avec les infos sur tous les livres
-3/à l'intérieur du dossier de la catégorie on créée dossier image_catégorie
--va à l'intérieur
-4/ds le dossier image_Catégorie, pour chaque livre : on télécharge l'image
-
-5/a chaque tour de boucle on remonte de 2 crans et on repart
-    -books_toscrap
-    -catégorie
-    -image_catégorie
-"""
-
-
-       
      
